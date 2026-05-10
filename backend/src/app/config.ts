82f896a3b19538a449b49849098b3daab9cb7226
TheRealShreyash <koshta.shreyash02@gmail.com>
2026-05-10 16:29:58 +0530

    add: config file with zod validation

---
+++ backend/src/app/config.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  IRIS_AUTH_URL: z.url(),
+  CLIENT_ID: z.string().min(1),
+  CLIENT_SECRET: z.string().min(1),
+  NODE_ENV: z.enum(["development", "production"]).default("development"),
+  FRONTEND_URL: z.url(),
+  PORT: z.number().default(8080),
+});
+
+const env = envSchema.parse(process.env);
+
+export const {
+  IRIS_AUTH_URL,
+  CLIENT_ID,
+  CLIENT_SECRET,
+  NODE_ENV,
+  FRONTEND_URL,
+  PORT,
+} = env;
